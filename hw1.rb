2d75dad7f2dd6577fd38fcd336d3586a53375a7e
Melissa Ly <melissakly@berkeley.edu>
2016-09-21 12:18:09 -0700

    /tentative/ finish of Q2 and Q4

--- hw1.rb
+++ hw1.rb
@@ -19,6 +19,17 @@ def combine_name(first_name, last_name)
 end
 
 def blockin_time(a)
+    stringtoint = irb(main):003:0> a.to_f
+    addtwo = stringtoint.map do |increment|
+        increment + 2
+        end
+    keepevens = addtwo.each { |value| puts value if value.to_f%2==0}
+    uniquearray = keepevens.uniq
+    for i in uniquearray
+        if i < 10
+            puts i
+            end
+        end
   # DO NOT EDIT THIS CODE BELOW
   require './foobar'
   Foobar.baz a
