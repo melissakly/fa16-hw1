8e8df4c3ea3a903d694b40f48de4a781911db551
Melissa Ly <melissakly@berkeley.edu>
2016-09-21 11:57:11 -0700

    finished Q1 and Q3,

--- hw1.rb
+++ hw1.rb
@@ -1,18 +1,21 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-  (a + b) ** 2
+  (b + a) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-  a.sort!.map do |b|
-    b + 1
-  end
+  #a.sort!.map do |b|
+  #b + 1
+  sort = a.sort
+  sortedone = sort.map do |element|
+      element + 1
+      end
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-  "#{first_name} #{last_name}"
+  puts "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
